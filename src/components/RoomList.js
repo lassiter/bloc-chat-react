a7fb13de08b6feebdfe97b58e161d879a9df9062
Lassiter Gregg <lassitergregg@gmail.com>
2018-01-03 22:21:33 -0600

    cp3

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -3,27 +3,55 @@ import React, { Component } from 'react';
 class RoomList extends Component {
   constructor(props) {
     super(props);
-      this.state = { rooms:[] };
+      this.state = { rooms:[], newRoomName:'' };
       this.roomsRef = this.props.firebase.database().ref('rooms');
+      this.handleChange = this.handleChange.bind(this);
+      this.createRoom = this.createRoom.bind(this);
   }
   componentDidMount() {
     this.roomsRef.on('child_added', snapshot => {
       const room = snapshot.val();
       room.key = snapshot.key;
-      this.setState({ rooms: this.state.rooms.concat( snapshot.val() ) });
       this.setState({ rooms: this.state.rooms.concat( room ) })
+      console.log(this.state.rooms)
     });
   }
+
+  handleChange(e) {
+    this.setState({ newRoomName: e.target.value })
+  }
+
+  createRoom() {
+    const room = { name: this.state.newRoomName };
+    this.roomsRef.push(room);
+    this.setState({ rooms: this.state.rooms.concat(room), newRoomName: "" });
+  }
+
+
   render() {
-    const roomList = this.state.rooms.map((room, i) =>
-      <li key={i} db-key={room.key}>{room.name}</li>
+  const roomList = this.state.rooms.map((room,i) =>
+      <li key={i}>{room.name}</li>
     )
+    console.log(this.state.rooms)
+    const roomForm =(
+      <form onSubmit={
+        (e) =>{
+          e.preventDefault();
+          e.stopPropagation();
+          this.createRoom();
+        }
+      }>
+        <input type="text" value={this.state.newRoomName} onChange={this.handleChange} placeholder="Name New Room"></input>
+        <button type="submit">Create</button>
+      </form>
+    );
+//onChange={this.setState({ newRoomName:this.value })}
     return (
       <div className="RoomList">
         <ul>{roomList}</ul>
+        <span>{roomForm}</span>
       </div>
     );
-    console.log(this.state);
   }
 }
 
