fd6c8bdf41a1782a0104b2e962efb8f70d28fb1a
Lassiter Gregg <lassitergregg@gmail.com>
2018-01-17 22:25:05 -0600

    cp5 final

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -3,44 +3,57 @@ import moment from 'moment';
 class MessageList extends React.Component {
   constructor(props) {
     super(props);
-      this.state = {};
+      this.state = {content: ""};
       this.messageRef = this.props.firebase.database().ref('messages');
       this.createMessage = this.createMessage.bind(this);
       this.handleNewMessage = this.handleNewMessage.bind(this);
+      this.canBeSubmitted = this.canBeSubmitted.bind(this)
   }
-  componentWillReceiveProps(){
-    const activeRoomSelected = this.props.activeRoom !== {} ? "false" : "true";
-    console.log("componentWillReceiveProps",this.props.activeRoom,activeRoomSelected)
+  createMessage(){
+
+    const message = {
+                        username: this.props.user,
+                        content: this.state.content,
+                        sentAt: this.props.firebase.database.ServerValue.TIMESTAMP,
+                        roomID: this.props.activeRoom.key
+      };
+      this.messageRef.push(message);
+      this.setState({
+        username: "",
+        content: "",
+        sentAt: "",
+        roomID: "",
+        textbox: 0
+      })
   }
 
-  createMessage(){
-    const message = {
-                      username: this.state.username,
-                      content: this.state.content,
-                      sentAt: this.state.sentAt,
-                      roomID: this.state.roomID
-    };
-    this.messageRef.push(message);
-    this.setState({
-      username: "",
-      content: "",
-      sentAt: "",
-      roomID: ""
-    })
+  canBeSubmitted(){
+    const {content} = this.state;
+    return (
+      content.length > 0 &&
+      Object.keys(this.props.activeRoom).length > 0
+    );
   }
   handleNewMessage(e) {
     this.setState({
-                    username: this.props.user,
-                    content: e.target.value,
-                    sentAt: this.props.firebase.database.ServerValue.TIMESTAMP,
-                    roomID: this.props.activeRoom.key
-                  })
+                    content: e.target.value
+                  }, () => console.log(this.state))
+
+  }
+  handleSubmit = (e) => {
+    e.preventDefault();
+    if(!this.canBeSubmitted()){
+        console.log(`!this.canBeSubmitted returned true`)
+        return;
+    }
+
+    this.createMessage();
+    console.log(`!this.canBeSubmitted returned false`)
   }
 
   render () {
-    const activeRoomSelected;
-    console.log(this.state, this.props.activeRoom, activeRoomSelected);
-
+    console.log(`Room key: ${this.props.activeRoom.key}, Room object is empty: ${Object.keys(this.props.activeRoom).length === 0}`);
+    const isDisabled = !this.canBeSubmitted();
     // console.log(this.props.activeRoomMessages)
     const activeRoomMessages = this.props.activeRoomMessages.map((message, i) =>
       <li key={i} className="msg-container">
@@ -54,9 +67,9 @@ class MessageList extends React.Component {
 
     let newMessageForm = (
         <span className="msg-input">
-        <form onSubmit={(e) =>{e.preventDefault();e.stopPropagation();this.createMessage();}}>
+        <form onSubmit={this.handleSubmit}>
           <textarea type="text" value={this.state.content} onChange={this.handleNewMessage}></textarea>
-          <button disabled={activeRoomSelected} type="submit">Submit</button>
+          <button disabled={isDisabled} type="submit">Submit</button>
         </form>
         </span>
       );
